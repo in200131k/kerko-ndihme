763867df894231d6ae64b1a31fb341f89a7b15bc
in200131k <in200131k@riinvest.net>
2023-03-09 22:58:02 +0100

    Add models

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Answer extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['description', 'user_id', 'question_id'];
+
+    public function votes(): MorphMany
+    {
+        return $this->morphMany(Vote::class, 'votable');
+    }
 }
